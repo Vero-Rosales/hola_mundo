7691aa2ec3977390e3b5cc18d00acae68a8d71d0
Verónica Rosales <veronica.rosales@ucuenca.edu.ec>
2024-09-26 18:07:08 -0500

    Se creó un cursor original en forma de estrella

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-function setup() {
+/*function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(50);
   background(0);
@@ -17,4 +17,45 @@ function draw() {
   stroke(0, 80, 0);
   fill(200, 60, 100);
   rect(mouseX, mouseY, 100, 100);
+}*/
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(60);
+  background(50);
+  ellipseMode(CENTER);
+}
+
+function draw() {
+  background(random(50, 150), 100, random(50, 150), 20);
+
+  stroke(random(100, 255), random(100, 255), 0);
+  strokeWeight(random(1, 15));
+  noFill();
+  triangle(
+    mouseX,
+    mouseY - 50,
+    mouseX - 50,
+    mouseY + 50,
+    mouseX + 50,
+    mouseY + 50
+  );
+
+  // Style the star shape.
+  noStroke();
+  fill(random(100, 255), 0, random(100, 255));
+
+  // Draw the star shape
+  let points = 5;
+  let outerRadius = 60;
+  let innerRadius = 30;
+  beginShape();
+  for (let i = 0; i < points * 2; i++) {
+    let angle = map(i, 0, points * 2, 0, TWO_PI);
+    let radius = i % 2 === 0 ? outerRadius : innerRadius;
+    let x = mouseX + cos(angle) * radius;
+    let y = mouseY + sin(angle) * radius;
+    vertex(x, y);
+  }
+  endShape(CLOSE);
 }
